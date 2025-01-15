2f4ab01e01c545f575f00270485f5bea243e208b
narendraba <ballipallinarendra@gmail.com>
2025-01-15 13:34:57 +0530

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,15 @@
+# modules/vpc/variables.tf
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+}
