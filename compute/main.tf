bdc05c6e8de0a928c192f921dc1c0d2f62804ca8
narendraba <ballipallinarendra@gmail.com>
2025-01-15 13:15:03 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "compute" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "compute-instance"
+  }
+}
