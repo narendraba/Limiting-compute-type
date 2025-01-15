f22cf69786087399480f19691223365d10f54139
narendraba <ballipallinarendra@gmail.com>
2025-01-15 16:50:18 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,17 +11,17 @@ resource "aws_subnet" "this" {
 #resource "aws_security_group" "this" {
 #  vpc_id = aws_vpc.this.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
