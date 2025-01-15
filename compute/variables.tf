deb8ad9a9fba36b33134b654de496dda55a414c9
narendraba <ballipallinarendra@gmail.com>
2025-01-15 13:15:43 +0530

    Create variables.tf

---
+++ compute/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
