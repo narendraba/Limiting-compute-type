594a6d7570a3666b930411b009c8e550124cb6f5
narendraba <ballipallinarendra@gmail.com>
2025-01-15 13:29:27 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,1 +1,27 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+}
 
+resource "aws_subnet" "this" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+}
+
+resource "aws_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
