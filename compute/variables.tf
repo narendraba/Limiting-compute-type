39385a87c119e9d81e9660c7ba63a720a13a8627
narendraba <ballipallinarendra@gmail.com>
2025-01-15 16:52:06 +0530

    Update variables.tf

--- compute/variables.tf
+++ compute/variables.tf
@@ -3,10 +3,10 @@ variable "instance_type" {
   type        = string
 }
 
-variable "subnet_id" {
-  description = "The subnet ID for the instance"
-  type        = string
-}
+#variable "subnet_id" {
+#  description = "The subnet ID for the instance"
+#  type        = string
+#}
 
 #variable "security_group_id" {
 #  description = "The security group ID for the instance"
