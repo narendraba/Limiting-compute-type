5682efb5c022fba486a1fdc37a661bdc1e70dca8
narendraba <ballipallinarendra@gmail.com>
2025-01-15 21:49:03 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "compute" {
   ami           = var.ami
   instance_type = var.instance_type
+  allowed_instance_types = var.allowed_instance_types
   #subnet_id     = var.subnet_id
   #security_groups = [var.security_group_id]
   tags = {
