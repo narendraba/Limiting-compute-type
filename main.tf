279a4de14ba51a6e20485ab08b5e4e79152ef40f
narendraba <ballipallinarendra@gmail.com>
2025-01-15 13:39:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,4 +17,6 @@ module "compute" {
   source = "./compute"
   ami           = var.ami
   instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = [var.security_group_id]
 }
